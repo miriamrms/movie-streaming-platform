f365a89b39b33c0d7b7eef333fa5deac85706762
Miri Mendes <miriam.rayanemendes@ufpe.br>
2026-05-16 00:21:27 +0000

    Add history Controller and Routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import historyRoutes from './routes/history-routes';
 
 const app = express();
 app.use(express.json());
@@ -6,6 +7,8 @@ app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
   res.json({ mensagem: "API funcionando!" });
 });
+
+app.use('/history', historyRoutes);
 
 app.listen(3000, () => {
   console.log("Servidor rodando em http://localhost:3000");
